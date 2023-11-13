d5203a73a3a1a7bacdd971835a99b3ca63a9d5a7
David Blair <dedge11@yahoo.com>
2023-11-12 19:23:39 -0500

    Added js

---
+++ app.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    var search = document.getElementById("btn-search");
+    var inputfield = document.getElementById("search-field-input");
+    var result = document.getElementById("result");
+    search.addEventListener("click", (e)=>{
+        e.preventDefault();
+        console.log("click");
+
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.open('GET', 'http:localhost/info2180-lab4/superheroes.php?query='+inputfield.value, true);
+        httpRequest.send();
+   
+        httpRequest.onreadystatechange = function() {
+            if(httpRequest.readyState === XMLHttpRequest.DONE){
+                if(httpRequest.status == 200) {
+                    var results = httpRequest.responseText;
+                   result.innerHTML=results
+            } else {
+                alert("Error");
+                }
+            }
+        }
+        
+    })}
